384e0448588523de2000a44e596ab3ce2e9c2044
Farid <sinabeyki93@gmail.com>
2024-09-26 01:41:22 +0330

    first commit

---
+++ data/about.js
@@ -0,0 +1,5 @@
+const data = {
+  heading: "Hello Everyone",
+  bio: "I'm Farid Beiki, a full-stack developer experienced in JavaScript, React, Next, Express, MongoDB, etc, I am a MERN Stack developer, I'm a self-learner web developer.",
+};
+export default data;
---
+++ data/header.js
@@ -0,0 +1,9 @@
+const data = {
+  image: "/images/profile3.jpg",
+  name: "Farid Beiki",
+  jobTitle: "full stack JS developer",
+  address: "Qom, Iran",
+  experience: "Almost 2 years of experience",
+  education: "Master of software engineering",
+};
+export default data;
